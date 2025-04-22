ade2633ed108a1c55b1f472bfc540fb371b2994e
JiwonRyu <jiwoni1210@naver.com>
2025-04-22 21:29:38 +0900

    [FE]Feat: 로그아웃 버튼 api 연동

--- frontend/src/components/mypage/LogoutButton.tsx
+++ frontend/src/components/mypage/LogoutButton.tsx
@@ -1,10 +1,16 @@
 'use client';
 
-import { Button } from '@/components/common';
+import { useRouter } from 'next/navigation';
 
+import { Button } from '@/components/common';
+import { useAuthStore } from '@/stores/authStore';
 const LogoutButton = () => {
+  const { logout } = useAuthStore.getState();
+  const router = useRouter();
+
   const handleLogout = () => {
-    console.log('로그아웃 버튼 클릭');
+    logout();
+    router.push('/');
   };
 
   return (
