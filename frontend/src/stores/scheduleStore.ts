ac5238d860c15e13fa004fdb6eec8faf1549e6da
JiwonRyu <jiwoni1210@naver.com>
2025-05-18 23:36:09 +0900

    [FE]Refactor: 일정 날짜 조회 startDate, endDate 전역 관리로 변경

--- frontend/src/stores/scheduleStore.ts
+++ frontend/src/stores/scheduleStore.ts
@@ -1,5 +1,7 @@
 import { create } from 'zustand';
 
+import { getMonthRange } from '@utils/date';
+
 import { Schedule } from '@/types/api';
 
 type ValuePiece = Date | null;
@@ -10,6 +12,8 @@ interface ScheduleState {
   selectedDate: Value; // 캘린더에서 선택한 날짜
   openStatusDropdownId: string | null; // 드롭다운을 연 일정의 id
   filteredSchedules: Schedule[]; // 캘린더에서 선택한 날짜의 일정 목록
+  startDate: string | null; // 일정 조회 시작일(YYYY-MM-DD)
+  endDate: string | null; // 일정 조회 종료일(YYYY-MM-DD)
 
   // openCalendarModal: (date: Date) => void; // 캘린더 모달 열기
 
@@ -18,6 +22,8 @@ interface ScheduleState {
   setOpenStatusDropdownId: (id: string | null) => void; // 드롭다운을 연 일정의 id 설정
 
   setSelectedDate: (date: Value) => void; // 캘린더에서 선택한 날짜 설정
+
+  setDateRange: (startDate: string, endDate: string) => void; // 일정 조회 범위 설정
 
   setFilteredSchedules: (schedules: Schedule[]) => void; // 필터링된 일정 목록 직접 설정
   // 일정 항목을 업데이트하는 함수
@@ -38,6 +44,8 @@ export const useScheduleStore = create<ScheduleState>((set) => ({
   selectedDate: null,
   openStatusDropdownId: null,
   filteredSchedules: [],
+  startDate: getMonthRange()[0],
+  endDate: getMonthRange()[1],
 
   setSelectedDate: (date: Value) => set({ selectedDate: date }),
 
@@ -58,6 +66,8 @@ export const useScheduleStore = create<ScheduleState>((set) => ({
     set(() => ({
       openStatusDropdownId: id,
     })),
+
+  setDateRange: (startDate: string, endDate: string) => set({ startDate, endDate }),
 
   // 필터링된 일정 목록 직접 설정
   setFilteredSchedules: (schedules) =>
